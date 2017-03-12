eb45c5e92d26fd200c4a1220eca7bf989a2f5e82
jaugeni <jaugeni@gmail.com>
2017-03-12 13:21:29 -0400

    UI setup

--- meme editor/ViewController.swift
+++ meme editor/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var cameraBtmOutlet: UIBarButtonItem!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func cameraBtnPressed(_ sender: Any) {
+        
+    }
+    @IBAction func albumPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func shareBtnPressed(_ sender: Any) {
+        
+    }
+    
 
 }
 
