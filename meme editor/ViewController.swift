7c1eb1f1da06ad795509f3530c25796cb9d9f72d
jaugeni <jaugeni@gmail.com>
2017-03-12 13:56:28 -0400

    photo library added

--- meme editor/ViewController.swift
+++ meme editor/ViewController.swift
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextField: UITextField!
-    
     @IBOutlet weak var imageView: UIImageView!
-    
     @IBOutlet weak var cameraBtmOutlet: UIBarButtonItem!
     
     
@@ -22,22 +20,34 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: Photo library imlimatation
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
 
     @IBAction func cameraBtnPressed(_ sender: Any) {
-        
+
     }
+    
     @IBAction func albumPressed(_ sender: Any) {
-        
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        self.present(pickerController, animated: true, completion: nil)
     }
     
     @IBAction func shareBtnPressed(_ sender: Any) {
         
     }
+    
+    
     
 
 }
